61406c6b3f7f5acd4a17273c2375d308a1ebfa09
Jean Brigonnet <jeanbrigo@gmail.com>
2024-01-11 10:40:38 -0600

    updated readme

--- songs/models.py
+++ songs/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
 class Song(models.Model):
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=99)
     artist = models.CharField(max_length=100)
 # Create your models here.
